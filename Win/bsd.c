2c05b6a9cd28d5179cff4db43faf29a66b9324c7
David Kinder <david.kinder@david.kinder>
2021-06-13 15:46:48 +0100

    Simpler implementation of gettimeofday(), using timespec_get()

--- Win/bsd.c
+++ Win/bsd.c
@@ -1,26 +1,17 @@
 /* Implementation of BSD Unix functions needed by Glulxe
    that are missing from Visual C++. */
 
-#include <stdint.h>
+#include <time.h>
 #include <sys/time.h>
-
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
 
 int gettimeofday(struct timeval *tp, struct timezone *tzp)
 {
-  FILETIME sysTime;
-  GetSystemTimeAsFileTime(&sysTime);
+  struct timespec ts;
 
-  // System time in microseconds since 1601-01-01
-  uint64_t time = (uint64_t)sysTime.dwLowDateTime;
-  time += (uint64_t)sysTime.dwHighDateTime << 32;
-  time /= 10;
+  if (timespec_get(&ts,TIME_UTC) == 0)
+    return -1;
 
-  // System time in microseconds since 1970-01-01
-  time -= 11644473600000000ULL;
-
-  tp->tv_sec = (long)(time / 1000000);
-  tp->tv_usec = (long)(time % 1000000);
+  tp->tv_sec = ts.tv_sec;
+  tp->tv_usec = ts.tv_nsec / 1000;
   return 0;
 }
